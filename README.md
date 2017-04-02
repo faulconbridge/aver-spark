bc98a74491a05d68ef84c13956297bb51936b54e
faulconbridge <chris@chriswetherill.me>
2017-04-02 14:15:59 -0400

    Some basic setup items

---
+++ README.md
@@ -0,0 +1,19 @@
+= Requirements =
+
+  - Docker
+  - Python 3
+  - wget
+  - unzip
+  - git
+  - Java
+
+== To Run =
+
+To run,
+```
+$ cd path/to/aver-spark
+$ docker-compuse up -d
+$ ./init.sh
+```
+
+This will start the Docker container, download all external dependencies, and run a couple of Spark jobs. This will output two files: `./output/era.txt` and `./output/inningsPitched.txt`.
---
+++ init.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+set -e
+
+# Convenience functions to make horrible errors less frightening
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+########################################
+#
+# Check for dependencies
+#
+########################################
+
+hasPython3=$(which python3)
+if [ $? -eq 1 ]; then
+  fail "Are you sure you have Python3 installed?"
+fi
+
+hasWget=$(which wget)
+if [ $? -eq 1 ]; then
+  fail "Are you sure you have wget installed?"
+fi
+
+hasUnzip=$(which unzip)
+if [$? -eq 1 ]; then
+  fail "Are you sure you have unzip installed?"
+fi
+
+if [ ! -d "${SCRIPT_DIR}/data" ]; then
+  mkdir ${SCRIPT_DIR}/data
+fi
+
+if [ ! -d "${SCRIPT_DIR}/externals" ]; then
+  mkdir ${SCRIPT_DIR}/externals
+fi
+
+########################################
+#
+# Download data and some other fun
+# mysteries to surprise and delight
+# users
+#
+########################################
+
+wget http://seanlahman.com/files/database/lahman2012-csv.zip -O ${SCRIPT_DIR}/data/baseball.zip
+unzip ${SCRIPT_DIR}/data/baseball.zip -d ${SCRIPT_DIR}/data
+
+wget http://www.gtlib.gatech.edu/pub/apache/avro/avro-1.8.1/py3/avro-python3-1.8.1.tar.gz -O ${SCRIPT_DIR}/externals/avro.tar.gz
+
+if [ ! -d "${SCRIPT_DIR}/externals/avro" ]; then
+ mkdir ${SCRIPT_DIR}/externals/avro
+fi
+
+tar -xzvf ${SCRIPT_DIR}/externals/avro.tar.gz \
+  -C ${SCRIPT_DIR}/externals/avro \
+  --strip-components=1
+
+# Set up avro
+sudo python3 ${SCRIPT_DIR}/externals/avro/setup.py install
