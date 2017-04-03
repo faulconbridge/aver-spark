185793c8a2f7f408512808513d846deecce1af97
Christopher Wetherill <christopher.wetherill@safeauto.com>
2017-04-03 10:20:17 -0400

    Make things less fragile

--- init.sh
+++ init.sh
@@ -6,8 +6,6 @@
 #
 ################################################################################
 
-set -e
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Convenience functions to make horrible errors less frightening
@@ -35,25 +33,71 @@ fail () {
 #
 ################################################################################
 
-hasPython3=$(which python3)
+##########
+# PYTHON
+##########
+
+info "Checking whether Python3 is installed..."
+noPy3Alias=false
+hasPython3=$(which python3 &> /dev/null)
+
 if [ $? -eq 1 ]; then
-  fail "Are you sure you have Python3 installed?"
+  hasPython3=$(python -c "import sys;t='{v[0]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";)
+
+  if [ ${hasPython3} -eq 3 ]; then
+    noPy3Alias=true
+    success "Found Python3!"
+  else
+    fail "Are you sure you have Python3 installed?"
+  fi
+else
+  success "Found Python3!"
 fi
 
-hasWget=$(which wget)
+##########
+# WGET / CURL
+##########
+
+useCurl=false
+info "Checking whether wget is installed..."
+hasWget=$(which wget &> /dev/null)
 if [ $? -eq 1 ]; then
-  fail "Are you sure you have wget installed?"
+  info "Hmm... we couldn't find wget."
+  info "We'll try to use curl instead..."
+  hasCurl=$(which curl &> /dev/null)
+
+  if [ $? -eq 1 ]; then
+    fail "Are you sure you have wget or curl installed?"
+  else
+    success "Found curl!"
+    useCurl=true
+  fi
+else
+  success "Found wget!"  
 fi
 
-hasUnzip=$(which unzip)
-if [$? -eq 1 ]; then
+##########
+# UNZIP
+##########
+
+info "Checking whether unzip is installed..."
+hasUnzip=$(which unzip &> /dev/null)
+if [ $? -eq 1 ]; then
   fail "Are you sure you have unzip installed?"
+else
+  success "Found unzip!"
 fi
 
+##########
+# Required directories
+##########
+
+info "Creating your data directory..."
 if [ ! -d "${SCRIPT_DIR}/data" ]; then
   mkdir ${SCRIPT_DIR}/data
 fi
 
+info "Creating your externals directory..."
 if [ ! -d "${SCRIPT_DIR}/externals" ]; then
   mkdir ${SCRIPT_DIR}/externals
 fi
@@ -66,15 +110,36 @@ fi
 #
 ################################################################################
 
-wget http://seanlahman.com/files/database/lahman2012-csv.zip \
-  -O ${SCRIPT_DIR}/data/baseball.zip
+##########
+# Download data
+##########
+
+info "Downloading and extracting your datasets..."
+if [ "$useCurl" = true ]; then
+  curl http://seanlahman.com/files/database/lahman2012-csv.zip \
+    -o ${SCRIPT_DIR}/data/baseball.zip
+else
+  wget http://seanlahman.com/files/database/lahman2012-csv.zip \
+    -O ${SCRIPT_DIR}/data/baseball.zip
+fi
 
 unzip ${SCRIPT_DIR}/data/baseball.zip \
   -d ${SCRIPT_DIR}/data
+success "Datasets successfully downloaded and unzipped!"
 
+##########
 # Download Avro
-wget http://www.gtlib.gatech.edu/pub/apache/avro/avro-1.8.1/py3/avro-python3-1.8.1.tar.gz \
-  -O ${SCRIPT_DIR}/externals/avro.tar.gz
+##########
+
+info "Downloading external dependencies..."
+# Download Avro
+if [ "$useCurl" = true ]; then
+  curl http://www.gtlib.gatech.edu/pub/apache/avro/avro-1.8.1/py3/avro-python3-1.8.1.tar.gz \
+    -o ${SCRIPT_DIR}/externals/avro.tar.gz
+else
+  wget http://www.gtlib.gatech.edu/pub/apache/avro/avro-1.8.1/py3/avro-python3-1.8.1.tar.gz \
+    -O ${SCRIPT_DIR}/externals/avro.tar.gz
+fi
 
 if [ ! -d "${SCRIPT_DIR}/externals/avro" ]; then
  mkdir ${SCRIPT_DIR}/externals/avro
@@ -84,5 +149,14 @@ tar -xzvf ${SCRIPT_DIR}/externals/avro.tar.gz \
   -C ${SCRIPT_DIR}/externals/avro \
   --strip-components=1
 
+##########
 # Install Avro
-sudo python3 ${SCRIPT_DIR}/externals/avro/setup.py install
+##########
+
+if [ "$noPy3Alias" = true ]; then
+  sudo python ${SCRIPT_DIR}/externals/avro/setup.py install
+else
+  sudo python3 ${SCRIPT_DIR}/externals/avro/setup.py install
+fi
+
+success "Successfully downloaded and installed Avro!"
